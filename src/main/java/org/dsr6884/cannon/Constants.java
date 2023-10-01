044a6beb2088b700cf1bb78139bb6698986299cd
LucasF6 <fowler.33148@sbhsd.k12.ca.us>
2023-10-01 11:12:21 -0700

    init

---
+++ src/main/java/org/dsr6884/cannon/Constants.java
@@ -0,0 +1,25 @@
+package org.dsr6884.cannon;
+
+/**
+ * Constants should be declared as public static final. When you are using your constants in a
+ * subsystem, you should use a static import.
+ * <p>
+ * E.g.
+ * <p>
+ * {@code import static org.dsr6884.cannon.Constants.Drive.*;}
+ */
+public final class Constants {
+  
+  public static final class Drive {
+
+  }
+
+  public static final class Shooter {
+
+  }
+
+  public static final class Turret {
+
+  }
+
+}
---
+++ src/main/java/org/dsr6884/cannon/subsystems/TankSubsystem.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.dsr6884.cannon.subsystems;
+
+public class TankSubsystem extends DriveSubsystem {
+  /** Creates a new TankSubsystem. */
+  public TankSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
